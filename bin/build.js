c4c179ffc3ccfe97e83a19c547340a014a77df26
Trey Shugart <treshugart@gmail.com>
2019-04-17 10:32:52 +1000

    Fix build and add npm scripts.

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,7 @@
 const { clean, client, config, entry, server } = require("..");
 
 module.exports = async function(opt) {
-  opt = config(opt);
+  opt = await config(opt);
   process.env.NODE_ENV = opt.env;
   await clean(opt);
   await entry(opt);
